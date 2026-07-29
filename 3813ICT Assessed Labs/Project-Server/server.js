62fd4d0bfdd67077c9fcf9d802b0c65849150bed
Anoraxium1 <267790205+Anoraxium1@users.noreply.github.com>
2026-07-30 09:52:25 +1000

    Added more info into files

--- 3813ICT Assessed Labs/Project-Server/server.js
+++ 3813ICT Assessed Labs/Project-Server/server.js
@@ -9,3 +9,7 @@ let server = http.listen(3000, function () {
     console.log("My First Nodejs Server!");
     console.log("Server listening on: " + host + " port: " + port);
 });
+
+app.get('/test', function (req, res) {
+    res.sendFile(__dirname + '/www/test.html');
+});

8785f45f0eb23585b848e7b97451303fdf5a3c75
Anoraxium1 <267790205+Anoraxium1@users.noreply.github.com>
2026-07-30 11:14:07 +1000

    Fixed the webpages and connected them together

--- 3813ICT Assessed Labs/Project-Server/auth.js
+++ 3813ICT Assessed Labs/Project-Server/auth.js
@@ -1,18 +1,28 @@
 var express = require('express'); // Used for routing
 var router = express.Router();
 
-router.use(express.urlencoded({ extended: true }));
+router.use(express.urlencoded({ extended: true}));
 
-var dummyUser = {
-    email: 'test@test.com',
-    password: '1234'
-};
+var dummyUsers = [
+    {email: 'test@test.com',password: '1234'},
+    {email: 'test1@test.com',password: '1234'},
+    {email: 'test2@test.com',password: '1234'}
+]
 
-router.post('/login', function (req, res) {
-    if (req.body.email === dummyUser.email && req.body.password === dummyUser.password) {
-        res.send('Login successful');
+router.post('/login/attempt', function (req, res) {
+    var matched = false;
+
+    for (var i = 0; i < dummyUsers.length; i++) {
+        if (req.body.email === dummyUsers[i].email && req.body.password === dummyUsers[i].password) {
+            matched = true;
+            break;
+        }
+    }
+
+    if (matched) {
+        res.json({ ok: true });
     } else {
-        res.status(401).send('Invalid Credentials');
+        res.json({ ok: false, errors: {} });
     }
 });
 
